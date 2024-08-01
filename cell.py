749a40eac1d4b111ea4cc8d3ec8b615a2cf53fe7
ProgrammerPouya <pouyanouri8203@gmail.com>
2024-07-31 21:41:28 -0700

    Instantiated cell class

--- cell.py
+++ cell.py
@@ -1,2 +1,8 @@
 class Cell:
-    def __init__(self, ):
+    
+    def __init__(self, coord:list, terrain:int, sprite:str, visibility:int):
+        self.coord = coord
+        self.terrain = terrain
+        self.sprite = sprite
+        self.visibility = visibility
+        
