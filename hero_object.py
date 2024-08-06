8ca4048a81e7ed4fc8f15e7af8b76c15853bb6b1
andrewyoung32 <andrewy@pdx.edu>
2024-08-05 20:33:45 -0700

    Update hero_object.py

--- hero_object.py
+++ hero_object.py
@@ -7,7 +7,7 @@ class hero:
     def __init__(self):
         self.energy = 100 #Starting energy
         self.whiffles = 1000 #Starting whiffles
-        self.inventory = ["","",""] #3 item inventory
+        self.inventory = [] #Empty inventory
         self.items = 0
         #self.diamonds = 0 #Royal Diamond count
     
@@ -18,19 +18,10 @@ class hero:
     
     
     def update_inventory(self,item_to_add):
-        if self.items == 3:
-            self.change_item(self,item_to_add)
-        
-        if self.items == 0:
-            self.inventory[0] = item_to_add
-            self.items += 1
-        elif self.items == 1:
-            self.inventory[1] = item_to_add
-            self.items += 1
-        elif self.items == 2:
-            self.inventory[2] = item_to_add
-            self.items += 1
-    
+        self.inventory.append(item_to_add)
+        self.items += 1
+        return True
+    '''
     def change_item(self,item_to_add):
         #Will need to ask them if they want to leave an item
         #0 for no 1 for first item, 2 for second item, 3 for third item
@@ -43,7 +34,7 @@ class hero:
         elif decision == 2:
             self.inventory[2] = item_to_add
         elif decision == 3:
-            self.inventory[3] = item_to_add
+            self.inventory[3] = item_to_add'''
         
     def buy_tool(self,tool,price):
         if (self.whiffles - price) > -1:
@@ -52,7 +43,23 @@ class hero:
             return True
         else:
             return False
+    
+    def use_item(self,item_needed):
+        if len(self.inventory) == 0:
+            return False
         
+        itm_index= None
+        try:
+            itm_index = self.inventory.index(item_needed)
+        except:
+            return False
+        
+        self.inventory.pop(itm_index)
+        return True
+        
+
+
+
         
     
         
